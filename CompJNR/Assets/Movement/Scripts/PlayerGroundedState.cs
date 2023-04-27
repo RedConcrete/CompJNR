d6476779e4524f48f71fc3abf500b0c3a325b198
RedConcrete <58186633+RedConcrete@users.noreply.github.com>
2023-04-27 15:19:52 +0200

    added Camera Fixed bugs

--- CompJNR/Assets/Movement/Scripts/PlayerGroundedState.cs
+++ CompJNR/Assets/Movement/Scripts/PlayerGroundedState.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerGroundedState : PlayerBaseState
+public class PlayerGroundedState : PlayerBaseState, IRootState
 {
   public PlayerGroundedState(PlayerStateMachine currentContext, PlayerStateFactory playerStateFactory)
   : base (currentContext, playerStateFactory) {
     IsRootState = true;
-    InitializeSubState();
+  }
+
+  public void HandleGravity() {
+    Ctx.CurrentMovementY = Ctx.Gravity;
+    Ctx.AppliedMovementY = Ctx.Gravity;
   }
 
   public override void EnterState(){
-      Ctx.CurrentMovementY = Ctx.GroundedGravity;
-      Ctx.AppliedMovementY = Ctx.GroundedGravity;
+    InitializeSubState();
+    HandleGravity();
   }
 
   public override void UpdateState(){
@@ -20,6 +24,17 @@ public class PlayerGroundedState : PlayerBaseState
   }
 
   public override void ExitState(){}
+
+  public override void CheckSwitchStates(){
+    // if player is grounded and jump is pressed, switch to jump state
+    if (Ctx.IsJumpPressed && !Ctx.RequireNewJumpPress) {
+      SwitchState(Factory.Jump());
+    }
+    // if player is not grounded and jump is not pressed, switch to fall state
+    else if (!Ctx.CharacterController.isGrounded) {
+      SwitchState(Factory.Fall());
+    }
+  }
 
   public override void InitializeSubState(){
     if (!Ctx.IsMovementPressed && !Ctx.IsRunPressed) {
@@ -30,11 +45,10 @@ public class PlayerGroundedState : PlayerBaseState
       SetSubState(Factory.Run());
     }
   }
-
-  public override void CheckSwitchStates(){
-    // if player is grounded and jump is pressed, switch to jump state
-    if (Ctx.IsJumpPressed && !Ctx.RequireNewJumpPress) {
-      SwitchState(Factory.Jump());
-    }
-  }
 }
+
+
+
+
+
+
