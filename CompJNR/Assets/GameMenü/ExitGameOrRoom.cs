fd6395975c0f1d3613b7660a224603ec3bc6c94c
RedConcrete <58186633+RedConcrete@users.noreply.github.com>
2023-05-05 11:37:39 +0200

    all working and there is now a gamemenu

---
+++ CompJNR/Assets/GameMenü/ExitGameOrRoom.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class ExitGameOrRoom : MonoBehaviourPunCallbacks
+{
+
+    PlayerInput _playerInput;
+    public GameObject gameMenu;
+    public int sceneID;
+
+    private void Awake()
+    {
+
+        _playerInput = new PlayerInput();
+        _playerInput.CharacterControls.Enable();
+
+        _playerInput.CharacterControls.Exit.performed += e => OnEscapeShowGameMenu();
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+
+    }
+
+    public override void OnLeftRoom()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != sceneID) // check if scene is already loaded
+        {
+            PhotonNetwork.LoadLevel(sceneID);
+        }
+        else
+        {
+            Debug.LogError("Scene still activ: " + sceneID);
+        }
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
+    void OnEscapeShowGameMenu()
+    {
+        gameMenu.SetActive(!gameMenu.activeSelf);
+    }
+
+}
