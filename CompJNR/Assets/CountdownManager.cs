65c4b7f4b6252fa479cd6f2807aa71bf76a8c9a8
RedConcrete <58186633+RedConcrete@users.noreply.github.com>
2023-06-21 23:31:55 +0200

    Hope its all there

---
+++ CompJNR/Assets/CountdownManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownManager : MonoBehaviour
+{
+    public int countdownTime;
+    private TMP_Text countdownDisplay;
+    private GameObject player;
+    private CharacterController characterController;
+
+    private void Start()
+    {
+        player = GameObject.Find("MarioWithMovemnetAndCameraMultiplayer");
+        countdownDisplay = GetComponent<TMP_Text>();
+        characterController = player.GetComponent<CharacterController>();
+        characterController.enabled = false;
+        StartCoroutine(CountdownToStart());
+    }
+
+    IEnumerator CountdownToStart()
+    {
+        while (countdownTime > 0) { 
+        countdownDisplay.text = countdownTime.ToString();
+            yield return new WaitForSeconds(1f);
+            countdownTime--;
+        }
+
+        countdownDisplay.text = "GOOO!!!";
+
+        characterController.enabled = true;
+
+        yield return new WaitForSeconds(1f);
+        countdownDisplay.gameObject.SetActive(false);
+    }
+
+}
