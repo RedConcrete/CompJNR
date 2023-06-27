e31ba573087a40567acc4058391af15a53d70dae
D4rkm4n215 <59933790+D4rkm4n215@users.noreply.github.com>
2023-06-27 18:40:29 +0200

    Items Movable final

--- CompJNR/Assets/Maker/Scripts/BrickHardButton.cs
+++ CompJNR/Assets/Maker/Scripts/BrickHardButton.cs
@@ -7,8 +7,20 @@ public class BrickHardButton : MonoBehaviour
     public MakerManager makerManager;
     private void OnMouseUpAsButton()
     {
-        makerManager.SelectObject(0);
-        Debug.Log("Brick has been Clicked");
-
+        if (this.gameObject.name == "Box")
+        {
+            makerManager.SelectObject(0);
+            Debug.Log("Box has been clicked!!");
+        }
+        if (this.gameObject.name == "Box Framed")
+        {
+            makerManager.SelectObject(1);
+            Debug.Log("Box Framed has been clicked!!");
+        }
+        if (this.gameObject.name == "Brick Hard")
+        {
+            makerManager.SelectObject(2);
+            Debug.Log("Brick Hard has been clicked!!");
+        }
     }
 }
--- CompJNR/Assets/Maker/Scripts/MakerManager.cs
+++ CompJNR/Assets/Maker/Scripts/MakerManager.cs
@@ -24,6 +24,60 @@ public class MakerManager : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if(pendingObject != null)
+            {
+                placedObjects.Remove(pendingObject);
+                PhotonNetwork.Destroy(pendingObject);
+                Destroy(pendingObject);
+            }
+            SelectObject(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (pendingObject != null)
+            {
+                placedObjects.Remove(pendingObject);
+                PhotonNetwork.Destroy(pendingObject);
+                Destroy(pendingObject);
+            }
+            SelectObject(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (pendingObject != null)
+            {
+                placedObjects.Remove(pendingObject);
+                PhotonNetwork.Destroy(pendingObject);
+                Destroy(pendingObject);
+            }
+            SelectObject(2);
+        }
+
+
+
+        if (pendingObject == null)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit))
+            {
+                var selection = hit.transform.gameObject;
+                if (Input.GetMouseButtonDown(1) && selection.CompareTag("MakerObject"))
+                {
+                    placedObjects.Remove(selection);
+                    PhotonNetwork.Destroy(selection);
+                    if(selection.name.Equals("Box(Clone)"))
+                        SelectObject(0);
+                    if (selection.name.Equals("Box Framed(Clone)"))
+                        SelectObject(1);
+                    if (selection.name.Equals("Brick Hard(Clone)"))
+                        SelectObject(2);
+                }
+            }
+        }
+
+
         if(pendingObject != null)
         {
             pendingObject.transform.position = new Vector3(
@@ -43,6 +97,12 @@ public class MakerManager : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.C))
             {
                 yOffset-= 2;
+            }
+            if (Input.GetKeyDown(KeyCode.Delete))
+            {
+                placedObjects.Remove(pendingObject);
+                PhotonNetwork.Destroy(pendingObject);
+                Destroy(pendingObject);
             }
         }
         if (Input.GetMouseButtonDown(0))
